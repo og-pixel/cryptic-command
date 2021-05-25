dd77b51a5c834f19c858f20f2417c661c2edea6e
Milosz Jakubanis <milosz.jakubanis@protonmail.com>
2021-05-25 11:54:19 +0100

    Removed unnecessary changes

--- scala3-server/src/main/scala/com/miloszjakubanis/crypticcommand/users/functions/AddArticleFunction.scala
+++ scala3-server/src/main/scala/com/miloszjakubanis/crypticcommand/users/functions/AddArticleFunction.scala
@@ -4,23 +4,11 @@ import com.miloszjakubanis.crypticcommand.users.User
 import com.miloszjakubanis.crypticcommand.articles.Article
 
 
-abstract class AddArticleFunction extends UserFunction[Article[_], Option[Article[_]]]:
+//TODO maybe delete
+class AddArticleFunction extends UserFunction[Article[_], Option[Article[_]]]:
   def apply(args: Article[_])(using User): Option[Article[_]] = AddArticleFunction(args)
 
 
 object AddArticleFunction extends UserFunction[Article[_], Option[Article[_]]]:
   def apply(arg: Article[_])(using user: User): Option[Article[_]] = 
     user.storage().put("", arg)
-
-
-object SuperAddArticleFunction extends AddArticleFunction:
-  override def apply(args: Article[_])(using User): Option[Article[_]] =
-    val result = AddArticleFunction(args)
-    println("I have used special function")
-    result
-
-class SuperAddArticleFunction extends AddArticleFunction:
-  override def apply(args: Article[_])(using User): Option[Article[_]] =
-    val result = AddArticleFunction(args)
-    println("I have used special function")
-    result
